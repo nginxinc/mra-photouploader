22937ea11c57c56d260e6860c7693771d2469aab
Eduard Borges <eduardborges@me.com>
2018-02-16 20:36:20 +0100

    MRADEV-697 router-mesh support

--- app/start.sh
+++ app/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-NGINX_PID="/var/run/nginx.pid"    # /   (root directory)
 APP="pm2 start app.js"
-
-NGINX_CONF="/etc/nginx/nginx.conf";
 
 if [ "$NETWORK" = "fabric" ]
 then
+    NGINX_PID="/var/run/nginx.pid"    # /   (root directory)
+    NGINX_CONF="/etc/nginx/nginx.conf";
     echo fabric configuration set;
+    nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;"
 fi
 
 if [ "$DEV_MODE" = "true" ]
@@ -17,15 +17,22 @@ fi
 npm install
 $APP 
 
-nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;"
-
 sleep 10
 #APP gets rendered as PM2
 APP=PM2
 APP_PID=`ps aux | grep "$APP" | grep -v grep`
 
-while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
-do 
-	sleep 5;
-	APP_PID=`ps aux | grep "$APP" | grep -v grep`;
-done
+if [ "$NETWORK" = "fabric" ]
+then
+    while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
+    do
+	    sleep 5;
+	    APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+    done
+else
+    while [ "$APP_PID" ];
+    do
+	    sleep 5;
+	    APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+    done
+fi
