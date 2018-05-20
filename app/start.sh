53b4e43e6d41bd8b6e7706e69f00a89dcd2b5579
Eduard Borges <eduardborges@me.com>
2018-05-20 08:57:20 +0200

    MRADEV-911 MRADEV-912 MRADEV-913

--- app/start.sh
+++ app/start.sh
@@ -3,18 +3,6 @@ NGINX_PID="/var/run/nginx.pid"    # /   (root directory)
 NGINX_CONF=""
 APP="pm2 start app.js"
 
-case "$NETWORK" in
-    fabric)
-        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
-        echo 'Fabric configuration set'
-        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
-        ;;
-    router-mesh)
-        ;;
-    *)
-        echo 'Network not supported'
-esac
-
 npm install
 
 if [ "$DEV_MODE" = "true" ]
@@ -29,8 +17,27 @@ sleep 10
 APP=PM2
 APP_PID=`ps aux | grep "$APP" | grep -v grep`
 
-while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
-do 
-	sleep 5;
-	APP_PID=`ps aux | grep "$APP" | grep -v grep`;
-done
+case "$NETWORK" in
+    fabric)
+        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
+        echo 'Fabric configuration set'
+        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
+
+        sleep 20
+        while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
+        ;;
+    router-mesh)
+        while [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
+        ;;
+    *)
+        echo 'Network not supported'
+        exit 1
+esac
